9e3fac43c52652beff5bb94cb822b01b16fb8628
TimProger <akylverentimofei@mail.ru>
2022-06-22 01:18:07 +0300

    url

--- src/config/index.js
+++ src/config/index.js
@@ -1,2 +1,2 @@
 export const SOCKET_URL = 'https://timprog-chat-api.herokuapp.com/'
-export const SERVER_URL = 'https://timprog-chat-api.herokuapp.com//api/v1'
+export const SERVER_URL = 'https://timprog-chat-api.herokuapp.com/api/v1'
